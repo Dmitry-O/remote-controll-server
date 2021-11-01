bbf5f1118a5998e2266d5a6db0f5b5ac1dec2b74
Dmitry-O <dimasic9420@gmail.com>
2021-11-01 21:01:14 +0200

    v.1.5.0

--- routes/cors.js
+++ routes/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 
-const whiteList = ['http://localhost:3000', 'https://remote-controllers.web.app/'];
+const whiteList = ['https://remote-controllers.web.app/', 'http://localhost:3000'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if(whiteList.indexOf(req.header('Origin')) !== -1) {
